33b49758b0b9ae889f87f2760a9d971ada43334e
Thomas Ballinger <thomasballinger@gmail.com>
2021-09-02 10:45:40 -0700

    so random seeds differently

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,9 @@
 import endless_sky_bindings as m
+import platform
 
 def test_random():
     m.RandomSeed(123);
-    assert m.RandomInt(10) == 5;
+    if platform.system() == 'Darwin':
+        assert m.RandomInt(10) == 5;
+    elif platform.system() == 'Linux':
+        assert m.RandomInt(10) == 3;
