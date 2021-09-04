ed60e92fe2d719f891c876a0d8673f5f05eb6cdd
Thomas Ballinger <thomasballinger@gmail.com>
2021-09-03 20:46:13 -0700

    fix

--- grab_libraries.py
+++ grab_libraries.py
@@ -44,9 +44,9 @@ elif platform.system() == 'Windows':
     DIR_MINGW64 = os.environ.get('DIR_MINGW64')
     assert DIR_MINGW64, "DIR_MINGW64 envar required"
     assert os.path.exists(DIR_MINGW64), "Need mingw installation at" + DIR_MINGW64
-    shutil.copy(os.path.join(DIR_MINGW64, 'lib\libgcc_s_seh-1.dll'), 'endless_sky_lib/')
-    shutil.copy(os.path.join(DIR_MINGW64, 'lib\libstdc++-6.dll'), 'endless_sky_lib/')
-    shutil.copy(os.path.join(DIR_MINGW64, 'lib\libwinpthread-1.dll'), 'endless_sky/lib.')
+    shutil.copy(os.path.join(DIR_MINGW64, 'lib\libgcc_s_seh-1.dll'), 'endless_sky/lib/')
+    shutil.copy(os.path.join(DIR_MINGW64, 'lib\libstdc++-6.dll'), 'endless_sky/lib/')
+    shutil.copy(os.path.join(DIR_MINGW64, 'lib\libwinpthread-1.dll'), 'endless_sky/lib/')
 
 else:
     assert False, "Platform not supported"
