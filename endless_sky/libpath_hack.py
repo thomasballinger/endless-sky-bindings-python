7156b0b67bd3894a37a32420624db449e6b38f2d
Thomas Ballinger <thomasballinger@gmail.com>
2021-09-03 20:00:42 -0700

    wont' help much

---
+++ endless_sky/libpath_hack.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import platform
+
+extra_dll_dir = os.path.join(os.path.dirname(__file__), 'lib')
+
+print('considering libpath hack...')
+if platform.system() == 'Windows':
+    if sys.version_info >= (3, 8):
+        os.add_dll_directory(extra_dll_dir)
+        print('called add_dll_directory on', extra_dll_dir)
+        print(os.path.listdir(extra_dll_dir))
+    else:
+        # legacy DLL loading mechanism through PATH env variable manipulations
+        os.environ.setdefault("PATH", "")
+        os.environ["PATH"] += os.pathsep + extra_dll_dir
